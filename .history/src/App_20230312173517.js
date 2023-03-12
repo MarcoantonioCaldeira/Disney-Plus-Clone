7289588e09cca0ccedba29a216dc1b4a40df996a
Marco Antonio <caldeiran391@gmail.com>
2023-03-12 17:41:18 -0300

    atualizando arquivos

--- .history/src/App_20230312173517.js
+++ .history/src/App_20230312173517.js
@@ -1,16 +1,10 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route crash path="/">
-
-          <Login />
-        </Route>
-      </Switch>
+      <Router />
     </div>
   );
 }
