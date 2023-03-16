3c31adaf1417e24f7e5ae469b8f1ce81336430c6
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-16 14:12:58 -0300

    Mexendo na estilização  e resolvendo os problemas de versão do router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route>
-        <Login />
-      </Route>
+      <Route exact path="/" element={<Login />}></Route>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 const Login = (props) => {
     return (
         <Container>
-            <Content>Content</Content>
+            <Content>
+
+            </Content>
         </Container>
     );
 };
@@ -23,13 +25,48 @@ const Content = styled.div`
     width: 100%;
     position: relative;
     min-heigth: 100vh;
-    box-sizing: border-box
+    box-sizing: border-box;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     padding: 80px 40px;
     heigth: 100%;
+`;
+
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+
+const CTA = styled.div`
+    margin-bottom: 2px;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center
+    margin-right: auto;   
+`;
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-heigth: 1px;
+  display: block;
+  width: 100%;
 `;
 
 
