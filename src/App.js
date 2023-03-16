78461c78e70bf940fb2c3081a3f4fc4c7a9f3b57
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-16 13:32:19 -0300

    Mexendo na estilização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "./components/Login";
 import './App.css';
 
