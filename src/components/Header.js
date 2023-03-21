3b22a49bd6789ccc3bbbddde648589e6b00169b7
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-21 20:46:20 -0300

    Criando o Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,67 @@ import styled from "styled-components";
 
 
 const Header = (props) => {
-
-    return <Nav>Header</Nav>;
+    return(
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt="Disney+" />
+            </Logo>
+            <NavMenu>
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt="HOME" />
+                </a>
+            </NavMenu>
+        </Nav>
+    );
 };
 
-const Nav = styled.Nav`
- position: fixed;
- top:0;
- left:0;
- right: 0;
- heigth: 70px;
- background-color: #090b13;
- display: flex;
- justify-content: space-between;
- align-items: center;
- padding: 0 36px;
- latter-spacing: 16px;
- z-index: 3;
+const Nav = styled.nav`
+    position: fixed;
+    top:0;
+    left:0;
+    right: 0;
+    heigth: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: 3;
 `;
+
+
+const Logo = styled.a`
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    font-size: 0;
+    display: inline-block;
+
+        img{
+            display: block;
+            width: 100%;
+        }
+`;
+
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    heigth: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-rigth: auto;
+    margin-left: 25px;
+
+    @media (max-width: 768px){
+        display: none;
+    }
+`;
+
 
 export default Header;
