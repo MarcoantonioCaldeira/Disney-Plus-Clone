7af47e10767269cc94e311ee33ad2b1dc8d76d92
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-21 20:00:54 -0300

    Criando o Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import react from 'react';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" element={<Login />}></Route>
+      <Header />   
+      <Login />
     </div>
   );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root')) 
+root.render(<App />);
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,14 @@ const Login = (props) => {
         <Container>
             <Content>
                 <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                        <SignUp>GET ALL THERE</SignUp>
+                        <Description>
+                            Get Premier Access to Raya and the Last Dragon for an additional fee
+                            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                            and The Disney Bundle will increase by $1.
+                        </Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
                 </CTA>
                 <BgImage />
             </Content>
@@ -42,7 +50,7 @@ const BgImage = styled.div`
   background-position: top;
   background-size: cover;
   background-repeat: no-repeat;
-  background-image: url("/images/login-background.jpg");
+  background-image: url("./images/login-background.jpg");
   position: absolute;
   top: 0;
   right: 0;
@@ -62,6 +70,8 @@ const CTA = styled.div`
     align-items: center;
     text-align: center
     margin-right: auto;   
+    margin-left: auto;
+    transition-timing-
 `;
 
 
@@ -73,6 +83,37 @@ const CTALogoOne = styled.img`
   width: 100%;
 `;
 
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover {
+        background-color: #0483ee;
+    }
+`; 
 
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
 
 export default Login;
